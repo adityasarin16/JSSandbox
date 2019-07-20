bd648c7eab48f280fcfe2ea39585e7e24e9a868a
aditya <sarinaditya@yahoo.co.in>
2019-07-20 09:14:54 -0400

    javascript commit
    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//Log to console
+console.log("hello world");
+var greetings = "O M G";
+console.log(greetings);
+
+// To log in a table format
+console.table({a:1, b:2});
+
+//WARN
+console.warn("warn is yellow");
+
+//Time functionality: this would give you time it took to run the script
+console.time("time");
+console.log("this is the time taken");
+console.log("this is the time taken");
+console.log("this is the time taken");
+console.log("this is the time taken");
+console.log("this is the time taken");
+console.timeEnd("time");
+
